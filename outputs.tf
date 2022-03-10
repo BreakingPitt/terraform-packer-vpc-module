943c643a8e541286bf429afb15da1c15ef3bdece
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2022-03-10 16:18:19 +0100

    Fix outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "vpc_id" {
-  value       = aws_vpc.packer.id
+  value       = aws_vpc.this.id
   description = "VPC ID"
 }
 
 output "subnet_id" {
-  value       = aws_subnet.packer.id
+  value       = aws_subnet.this.id
   description = "Subnet ID"
 }
 
