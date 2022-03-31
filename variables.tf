5488bd32f3cfa218e43691d9e61656826c3bbadb
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2022-03-31 20:05:42 +0200

    Add required variables to variables.tf files

--- variables.tf
+++ variables.tf
@@ -4,19 +4,53 @@ variable "cidr_block" {
   type        = string
 }
 
-variable "environment" {
-  description = "Environment tag"
-  type        = string
+
+variable "enable" {
+  default     = true
+  description = "Flag to control the VPC creation."
+  type        = bool
 }
+
+
+variable "enable_flow_log" {
+  default     = false
+  description = "Enable VPC flow logs."
+  type        = bool
+}
+
 
 variable "name" {
   default     = "packer"
-  description = "The name of the image build environment"
+  description = "The VPC name."
   type        = string
 }
 
-variable "region" {
-  default     = "eu-west-1"
-  description = "The AWS region to deploy to"
+
+variable "namespace" {
+  default     = "Engine9.io"
+  description = "The namespace can be your organization name or abbreviation."
+  type        = string
+}
+
+
+variable "s3_bucket_arn" {
+  default     = ""
+  description = "ARN of the S3 Bucket used to store the VPC flow logs."
+  sensitive   = true
+  type        = string
+}
+
+
+variable "stage" {
+  default     = ""
+  description = "The stage, e.g. 'prod', 'staging', 'dev'"
+  sensitive   = true
+  type        = string
+}
+
+
+variable "traffic_type" {
+  default     = "ALL"
+  description = "Type of traffic to capture. Valid values: ACCEPT, REJECT, ALL."
   type        = string
 }
