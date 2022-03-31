4cee044b205e1dcd41bf0ca842d81f13df279db1
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2022-03-31 20:06:55 +0200

    Improve outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,23 @@
-output "vpc_id" {
-  value       = aws_vpc.this.id
-  description = "VPC ID"
+output "internet_gateway_id" {
+  value       = join("", aws_internet_gateway.default.*.id)
+  description = "The ID of the Internet Gateway."
 }
 
+
 output "subnet_id" {
-  value       = aws_subnet.this.id
+  value       = join("", aws_subnet.default.*.id)
   description = "Subnet ID"
+}
+
+
+output "vpc_cidr_block" {
+  value       = join("", aws_vpc.default.*.cidr_block)
+  description = "The CIDR block of the VPC."
+}
+
+
+output "vpc_id" {
+  value       = join("", aws_vpc.default.*.id)
+  description = "VPC ID"
 }
 
