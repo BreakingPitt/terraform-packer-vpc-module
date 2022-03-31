119e8e490c85c5068a7cc38a51ea2a3360b94f20
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2022-03-31 20:10:56 +0200

    Adapt main.tf on example folder

--- example/main.tf
+++ example/main.tf
@@ -1,7 +1,8 @@
 module "packer_vpc" {
-  source       = "../../packer_vpc/"
-  cidr_block   = "10.10.0.0/16"
-  environment  = "dev"
-  name         = "packer-test-build"
-  region       = "eu-central-1"
+  source          = "../../packer_vpc/"
+  cidr_block      = "10.10.0.0/16"
+  enable          = true
+  enable_flow_log = false
+  stage           = "Dev"
+  name            = "packer-test-build"
 }
