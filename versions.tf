a39ac18f7d49cada0c91dd5639437751ed9b76c0
Pedro Garcia Rodriguez <pedgarro@gmail.com>
2023-07-10 13:50:31 +0200

    Update Terraform dependencies versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,25 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = "~> 1.5.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.63"
+      version = "~> 5.6.2"
+    }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
     }
   }
 }
