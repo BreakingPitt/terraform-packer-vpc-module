4b72ec4feb76c048791720bda4132dc317d8b01b
Pedro Garcia Rodriguez <pedgarro@gmail.com>
2023-07-11 14:08:50 +0200

    Change module path on Terraform Packer Vpc Module example

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,5 @@
 module "packer_vpc" {
-  source          = "../../packer_vpc/"
+  source          = "../../terraform-packer-vpc-module/"
   cidr_block      = "10.10.0.0/16"
   enable          = true
   enable_flow_log = false
